123b27fd96ee72abd0bf5310853cb294f9e42aac
hyunkyu4569 <60914217+hyunkyu4569@users.noreply.github.com>
2021-11-29 17:50:00 +0900

    글로벌 예외처리

---
+++ dmaker/src/main/java/com/fastcampus/programming/dmaker/controller/CMakerController.java
@@ -0,0 +1,13 @@
+package com.fastcampus.programming.dmaker.controller;
+
+import com.fastcampus.programming.dmaker.dto.DMakerErrorResponse;
+import com.fastcampus.programming.dmaker.exception.DMakerException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class CMakerController {
+
+}
